324a1e562110aed73fbb5103312c888bbcdd5a30
rhilmy <rahadianhilmy@gmail.com>
2021-05-22 16:45:22 +0700

    2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-require('dotenv').config()
+require('dotenv/config')
 const app = express();
 
 var corsOptions = {
